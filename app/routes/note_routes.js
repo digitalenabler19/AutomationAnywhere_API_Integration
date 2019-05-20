0fe2df9178522651ae5b144835ffe30ded88d6a9
Digital Enabler19 <digitalenabler19@gmail.com>
2019-05-20 20:06:46 +0530

    commented console message

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     // You'll create your note here.
-      console.log(req.body);
+      //console.log(req.body);
     //res.send("hello")
    res.send(req.body.title);
   });
